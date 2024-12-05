1c8ef099a1d2a9c5c1c293685c7c7efcaaaa0d0e
Dan Torres <danstorre@gmail.com>
2024-12-05 19:42:21 -0400

    creates project.

---
+++ ExchangeRateChallenge/ExchangeRateChallenge/ExchangeRateChallenge.h
@@ -0,0 +1,18 @@
+//
+//  ExchangeRateChallenge.h
+//  ExchangeRateChallenge
+//
+//  Created by Daniel Torres on 12/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExchangeRateChallenge.
+FOUNDATION_EXPORT double ExchangeRateChallengeVersionNumber;
+
+//! Project version string for ExchangeRateChallenge.
+FOUNDATION_EXPORT const unsigned char ExchangeRateChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExchangeRateChallenge/PublicHeader.h>
+
+
