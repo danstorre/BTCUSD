50e67d44e3cd0aff060370d6492423d4d4d8a62d
Dan Torres <danstorre@gmail.com>
2024-12-05 19:42:41 -0400

    RemoteExchangeRateLoader init does not message httpclient.

--- ExchangeRateChallenge/ExchangeRateChallengeTests/ExchangeRateChallengeTests.swift
+++ ExchangeRateChallenge/ExchangeRateChallengeTests/ExchangeRateChallengeTests.swift
@@ -1,1 +1,24 @@
+import XCTest
 
+class RemoteExchangeRateLoader {
+    private let client: HTTPClientSpy
+    
+    init(client: HTTPClientSpy) {
+        self.client = client
+    }
+    
+}
+
+class HTTPClientSpy {
+    private(set) var loadMessageCallCount = 0
+}
+
+final class RemoteExchangeRateLoaderTests: XCTestCase {
+    
+    func test_init_doesNotMessageHTTPClient() {
+        let spy = HTTPClientSpy()
+        let _ = RemoteExchangeRateLoader(client: spy)
+        
+        XCTAssertEqual(spy.loadMessageCallCount, 0)
+    }
+}
