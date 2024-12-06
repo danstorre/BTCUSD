ca7329d68ec0ae290c2a36b989c345298ffad494
Dan Torres <danstorre@gmail.com>
2024-12-05 20:09:19 -0400

    on http client error delivers connectivity error.

--- ExchangeRateChallenge/ExchangeRateChallengeTests/ExchangeRateChallengeTests.swift
+++ ExchangeRateChallenge/ExchangeRateChallengeTests/ExchangeRateChallengeTests.swift
@@ -4,13 +4,19 @@ class RemoteExchangeRateLoader {
     private let client: HTTPClientSpy
     private let url: URL
     
+    enum Error: Swift.Error {
+        case noConnectivity
+    }
+    
     init(client: HTTPClientSpy, url: URL) {
         self.client = client
         self.url = url
     }
     
-    func load() {
-        client.getData(from: url)
+    func load(completion: @escaping (Error) -> Void) {
+        client.getData(from: url) { response in
+            completion(.noConnectivity)
+        }
     }
 }
 
@@ -19,9 +25,15 @@ class HTTPClientSpy {
         requestedURLs.count
     }
     private(set) var requestedURLs = [URL]()
+    private var completions = [(Error) -> Void]()
     
-    func getData(from url: URL) {
+    func getData(from url: URL, completion: @escaping (Error) -> Void) {
         requestedURLs.append(url)
+        completions.append(completion)
+    }
+    
+    func completesWith(error: Error, at index: Int = 0) {
+        completions[index](error)
     }
 }
 
@@ -40,9 +52,24 @@ final class RemoteExchangeRateLoaderTests: XCTestCase {
         let anyURL = URL(string: "http://anyURL.com")!
         let sut = RemoteExchangeRateLoader(client: spy, url: anyURL)
         
-        sut.load()
-        sut.load()
+        sut.load() { _ in }
+        sut.load() { _ in }
         
         XCTAssertEqual(spy.requestedURLs, [anyURL, anyURL])
+    }
+    
+    func test_load_onHTTPClientError_deliversConnectivityError() {
+        let spy = HTTPClientSpy()
+        let anyError = NSError(domain: "", code: 1)
+        let anyURL = URL(string: "http://anyURL.com")!
+        let sut = RemoteExchangeRateLoader(client: spy, url: anyURL)
+        
+        var receivedError: RemoteExchangeRateLoader.Error?
+        sut.load() { error in
+            receivedError = error
+        }
+        spy.completesWith(error: anyError)
+        
+        XCTAssertEqual(receivedError, .noConnectivity)
     }
 }
