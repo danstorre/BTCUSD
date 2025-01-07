c130862d737ce26a331b07ace26994e2b448b659
Dan Torres <danstorre@gmail.com>
2025-01-07 19:30:41 -0400

    checks throwing cache error, on deletion error.

--- ExchangeRateChallenge/ExchangeRateChallengeTests/CacheExchangeRateTests.swift
+++ ExchangeRateChallenge/ExchangeRateChallengeTests/CacheExchangeRateTests.swift
@@ -3,12 +3,20 @@ import XCTest
 class CacheExchangeRate {
     private let store: StoreSpy
     
+    enum Error: Swift.Error {
+        case deletionError(Swift.Error)
+    }
+    
     init(store: StoreSpy) {
         self.store = store
     }
     
-    func cache() {
-        store.delete()
+    func cache() throws {
+        do {
+            try store.delete()
+        } catch {
+            throw Error.deletionError(error)
+        }
     }
 }
 
@@ -21,8 +29,18 @@ class StoreSpy {
         case insertion
     }
     
-    func delete() {
+    var stubbedResult: Error?
+    
+    func delete() throws {
         messages.append(.deletion)
+        
+        if let stubbedResult {
+            throw stubbedResult
+        }
+    }
+    
+    func failsWithError() {
+        
     }
 }
 
@@ -36,12 +54,31 @@ final class CacheExchangeRateTests: XCTestCase {
     func test_onCache_deletesStore() {
         let (sut, spy) = makeSUT()
         
-        sut.cache()
+        try? sut.cache()
         
         XCTAssertEqual(spy.messages, [.deletion])
     }
     
+    func test_onCache_onDeletionError_deliversDeletionError() {
+        let (sut, spy) = makeSUT()
+        let anyError = createAnyError()
+        spy.stubbedResult = createAnyError()
+        
+        XCTAssertThrowsError(try sut.cache()) { error in
+            if case CacheExchangeRate.Error.deletionError(let error) = error {
+                XCTAssertEqual((error as NSError).domain, (anyError).domain)
+                XCTAssertEqual((error as NSError).code, (anyError).code)
+            } else {
+                XCTFail()
+            }
+        }
+    }
+    
     // MARK: - Helpers
+    private func createAnyError() -> NSError {
+        return NSError(domain: "any", code: 1)
+    }
+    
     private func makeSUT(
         file: StaticString = #filePath,
         line: UInt = #line
